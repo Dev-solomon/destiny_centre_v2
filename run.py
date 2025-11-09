6c70ab5b8a500222c8377cd32cdaec53a612a9e7
Solomon Ntia <ntiasolomon7@gmail.com>
2025-11-09 07:54:39 -0500

    render port fixed

--- run.py
+++ run.py
@@ -47,4 +47,5 @@ def sermon_details(sermon_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))  # Render will supply PORT
+    app.run(host='0.0.0.0', port=port, debug=False)
